fa84ef0a6d8448472b305f744073466c6c79c587
apexsportsfinance-afk <apexsportsfinance@gmail.com>
2026-02-26 00:20:47 +0400

    Update Supabase URL and Anon Key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://dixelomafeobabahqeqg.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpeGVsb21hZmVvYmFiYWhxZXFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzMzA4MzYsImV4cCI6MjA4NjkwNjgzNn0.YD1lj0T6kFoM2XyeYonIC3bmLiPkKBvmXEHEr5VMaGM";
+const supabaseUrl = "https://urrdbdrnjhzquklrwfkd.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVycmRiZHJuamh6cXVrbHJ3ZmtkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzMTE1NDYsImV4cCI6MjA4Njg4NzU0Nn0.ijMY0OF5gQlQkQhBBI7zgbjKT0or0cINF6LLFgN0124";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
